11f05f26f75ec94345da3ab36f3e3ca923a720ec
Mira <najafi.mehraneh@gmail.com>
2022-11-22 17:48:42 +0330

    HW3 part 3, 4

---
+++ HW3/part 3, 4/BDD_3_3/src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+import java.lang.Math;
+
+public class Calculator {
+    public float divide(float a, float b){
+        return a / b;
+    }
+
+    public long squareRoot(float a){
+        return (long) Math.sqrt(a);
+    }
+
+}
